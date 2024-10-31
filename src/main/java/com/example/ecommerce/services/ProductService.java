d46b5c7fd64867b09cda6089c25a7c75eb6d3bf6
Danilo Sanna <sannadanilo98@gmail.com>
2024-10-31 23:16:29 +0100

    services

---
+++ src/main/java/com/example/ecommerce/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.example.ecommerce.services;
+
+import com.example.ecommerce.models.Product;
+import com.example.ecommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> findAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product findProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
