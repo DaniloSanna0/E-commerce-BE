4b2421ea26a4219b5ea5e497ac6af73523108802
Danilo Sanna <sannadanilo98@gmail.com>
2024-10-31 23:15:04 +0100

    services

--- src/main/java/com/example/ecommerce/services/UserService.java
+++ src/main/java/com/example/ecommerce/services/UserService.java
@@ -1,4 +1,21 @@
 package com.example.ecommerce.services;
 
+import com.example.ecommerce.models.User;
+import com.example.ecommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
 }
