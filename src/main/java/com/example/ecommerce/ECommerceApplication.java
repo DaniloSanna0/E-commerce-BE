a2208001584ba9079fa838720473c09f9fdb87a5
Danilo Sanna <sannadanilo98@gmail.com>
2024-11-03 21:29:43 +0100

    aggiunta del eccezione cors

--- src/main/java/com/example/ecommerce/ECommerceApplication.java
+++ src/main/java/com/example/ecommerce/ECommerceApplication.java
@@ -2,6 +2,9 @@ package com.example.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ECommerceApplication {
@@ -10,4 +13,15 @@ public class ECommerceApplication {
         SpringApplication.run(ECommerceApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permette il CORS su tutti gli endpoint
+                        .allowedOrigins("http://localhost:5173") // Consenti richieste da Vue
+                        .allowedMethods("GET", "POST", "PUT", "DELETE"); // Metodi consentiti
+            }
+        };
+    }
 }
